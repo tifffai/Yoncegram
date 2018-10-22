33b22982807cb99e6551a422009d5e01ff86c0d6
TF <tbluebox@gmail.com>
2018-10-22 17:17:28 +1100

    added photo upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -69,6 +69,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title, :url, :user_id)
+      params.require(:photo).permit(:title, :url, :user_id, :image)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :user
   has_many :comment
+  has_one_attached :image
 end
